2efd63144cf75f364ef5615ba2cd02bf39aafe63
BrenDev0 <lahey1991@gmail.com>
2025-07-16 17:49:41 -0600

    unused folder

--- core/api.py
+++ core/api.py
@@ -5,21 +5,21 @@ from modules.agents import agents_routes
 from modules.files import files_routes
 from core.middleware.auth_middleware import auth_middleware
 from core.app.config import Config  
+from core.dependencies.configure_container import configure_container
 
 config = Config
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await config.on_startup()  
+    configure_container()  
     yield
-    await config.on_shutdown() 
 
 app = FastAPI(lifespan=lifespan)
 
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://soulxae.up.railway.app"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
