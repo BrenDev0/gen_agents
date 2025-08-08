f0a5bb6cf93f260002bc95bebe7e4dc183695503
BrenDev0 <lahey1991@gmail.com>
2025-08-08 12:50:15 -0600

    moved deps to constructor

--- src/agent/services/state_service.py
+++ src/agent/services/state_service.py
@@ -6,12 +6,20 @@ from   typing import List
 from src.dependencies.container import Container
 from src.api.modules.chats.messages.messages_models import Message
 from src.api.modules.chats.messages.messages_service import MessagesService
+from src.api.modules.ai_config.ai_config_service import AiConfigService
 
 
 class StateService:
     _MODULE = "state.service"
-    def __init__(self, redis_service: RedisService):
+    def __init__(
+        self, 
+        redis_service: RedisService,
+        messages_service: MessagesService,
+        ai_config_service: AiConfigService
+    ):
         self._redis_service = redis_service
+        self._messages_service = messages_service
+        self._ai_config_service = ai_config_service
 
     @service_error_handler(module=_MODULE)
     async def get_state(self, db: Session, chat_id: UUID):
@@ -19,7 +27,7 @@ class StateService:
 
 
     @service_error_handler(module=_MODULE)
-    async def ___get_chat_history(self, db: Session, chat_id: UUID, num_of_messages: int = 12) -> List[Message]:
+    async def __get_chat_history(self, db: Session, chat_id: UUID, num_of_messages: int = 12) -> List[Message]:
         session_key = self._redis_service.get_agent_state_key(chat_id=chat_id)
     
         session_data = await self._redis_service.get_session(session_key)
@@ -27,8 +35,7 @@ class StateService:
         if session_data:
             chat_history = session_data.get("chat_history", [])
         else:
-            messages_service: MessagesService = Container.resolve("messages_service")
-            chat_history: List[Message] = messages_service.collection(db=db, chat_id=chat_id)[:num_of_messages]
+            chat_history: List[Message] = self._messages_service.collection(db=db, chat_id=chat_id)[:num_of_messages]
 
         if len(chat_history) != 0:
             return chat_history
@@ -37,7 +44,6 @@ class StateService:
     
     @service_error_handler(module=_MODULE)
     async def __get_ai_config(self, db: Session, agent_id: UUID):
-        ai_config_service = Container.resolve("ai_config_service")
         pass
     
 
